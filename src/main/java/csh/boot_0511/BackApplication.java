f70cd3760a76a538a60c9f85b4893ac5793dcc1f
devho <jshgold93@gmail.com>
2026-05-11 12:16:30 +0900

    init project

---
+++ src/main/java/csh/boot_0511/BackApplication.java
@@ -0,0 +1,13 @@
+package csh.boot_0511;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/csh/boot_0511/BackApplicationTests.java
@@ -0,0 +1,13 @@
+package csh.boot_0511;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
